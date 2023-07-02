83526698b0f2acbf01c4dfa1be39527810caa977
Serveresty <cs.go.12228@gmail.com>
2023-07-02 18:42:10 +0300

    csv/excel file fix

--- server/csv_upload_read.go
+++ server/csv_upload_read.go
@@ -3,11 +3,12 @@ package server
 import (
 	"AdminPanelCorp/database"
 	"AdminPanelCorp/utils"
-	"encoding/csv"
+	"bufio"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 
 	"github.com/gin-gonic/gin"
 )
@@ -24,29 +25,41 @@ func (db *DataBase) UploadUsers(c *gin.Context) {
 	filePath := fmt.Sprintf("./%s", fileObj.Filename) //Задавание пути csv файла
 	c.SaveUploadedFile(fileObj, filePath)             //Сохранение csv файла
 
-	records := ReadCSVFile(filePath)               //Считывание csv файла
+	records := readCSVFile(filePath)               //Считывание csv файла
 	data := database.CreateUsers(db.Data, records) //Отправление данных вида (email, username) в функцию создания пользователей
 	utils.Send_Email(data)                         //Отправление готовых данных в отправку сообщений на почты
-	eerr := os.Remove(filePath)                    //Удаление csv файла из Path
+
+	eerr := os.Remove(filePath) //Удаление csv файла из Path
 	if eerr != nil {
 		log.Fatal(eerr)
 	}
 	c.JSON(http.StatusOK, gin.H{"success": "all users has been added"})
 }
 
-// Функция, читающая CSV
-func ReadCSVFile(filePath string) [][]string {
-	f, err := os.Open(filePath) //Открытие csv файла
+// Функция, читающая файл с email и username
+func readCSVFile(filePath string) [][]string {
+	var line []string
+	var records [][]string
+	file, err := os.Open(filePath) //Открытие файла
 	if err != nil {
 		log.Fatal("Unable to read input file "+filePath, err)
 	}
-	defer f.Close()
+	defer file.Close()
 
-	csvReader := csv.NewReader(f)
-	records, err := csvReader.ReadAll() //Считывание csv файла
-	if err != nil {
-		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+	fileScanner := bufio.NewScanner(file)
+
+	//Сканирование файла построчно
+	for fileScanner.Scan() {
+		words := regexp.MustCompile("[,;\n]{1}").Split(fileScanner.Text(), -1)
+		for _, word := range words {
+			line = append(line, word)
+		}
+		records = append(records, line)
+		line = nil
 	}
 
-	return records //Возврат массива из структур с данными пользователей
+	if err := fileScanner.Err(); err != nil {
+		log.Fatalf("Error while reading file: %s", err)
+	}
+	return records
 }
