3f07be58ed1f1339501e55cbebc9a2466dedc55c
Serveresty <cs.go.12228@gmail.com>
2023-07-02 18:49:55 +0300

    added json errors

--- server/csv_upload_read.go
+++ server/csv_upload_read.go
@@ -5,7 +5,6 @@ import (
 	"AdminPanelCorp/utils"
 	"bufio"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"regexp"
@@ -15,34 +14,41 @@ import (
 
 // Функция, получающая файл из <input>
 func (db *DataBase) UploadUsers(c *gin.Context) {
-	fileObj, err := c.FormFile("filename") //Получение csv файла из html
+	fileObj, err := c.FormFile("filename") //Получение файла из html
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"err": err,
 		})
 		return
 	}
-	filePath := fmt.Sprintf("./%s", fileObj.Filename) //Задавание пути csv файла
-	c.SaveUploadedFile(fileObj, filePath)             //Сохранение csv файла
+	filePath := fmt.Sprintf("./%s", fileObj.Filename)   //Задание пути файла
+	err_saving := c.SaveUploadedFile(fileObj, filePath) //Сохранение файла
+	if err_saving != nil {
+		c.JSON(400, gin.H{"error": "Error while saving file"})
+	}
 
-	records := readCSVFile(filePath)               //Считывание csv файла
+	records, err := readCSVFile(filePath) //Считывание файла
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Error while reading file"})
+	}
+
 	data := database.CreateUsers(db.Data, records) //Отправление данных вида (email, username) в функцию создания пользователей
 	utils.Send_Email(data)                         //Отправление готовых данных в отправку сообщений на почты
 
 	eerr := os.Remove(filePath) //Удаление csv файла из Path
 	if eerr != nil {
-		log.Fatal(eerr)
+		c.JSON(400, gin.H{"error": "Error while deleting file"})
 	}
 	c.JSON(http.StatusOK, gin.H{"success": "all users has been added"})
 }
 
 // Функция, читающая файл с email и username
-func readCSVFile(filePath string) [][]string {
+func readCSVFile(filePath string) ([][]string, error) {
 	var line []string
 	var records [][]string
 	file, err := os.Open(filePath) //Открытие файла
 	if err != nil {
-		log.Fatal("Unable to read input file "+filePath, err)
+		return nil, err
 	}
 	defer file.Close()
 
@@ -59,7 +65,7 @@ func readCSVFile(filePath string) [][]string {
 	}
 
 	if err := fileScanner.Err(); err != nil {
-		log.Fatalf("Error while reading file: %s", err)
+		return nil, err
 	}
-	return records
+	return records, nil
 }
